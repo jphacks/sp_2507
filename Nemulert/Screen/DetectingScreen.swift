7f80347764e517934dcc615139ea9d50f2cccaad
Ayaka Hosoda <ayaka.hosoda@icloud.com>
2025-10-18 11:44:27 +0900

    Add DetectingScreen

---
+++ Nemulert/Screen/DetectingScreen.swift
@@ -0,0 +1,18 @@
+//
+//  DetectingScreen.swift
+//  Nemulert
+//
+//  Created by 細田彩香 on 2025/10/18.
+//
+
+import SwiftUI
+
+struct DetectingScreen: View {
+    var body: some View {
+        LottieView(name: "Nemulert")
+    }
+}
+
+#Preview {
+    DetectingScreen()
+}
