e4c5411f4c0ea179f39d0b92b16cf7d149756e21
Kanta Oikawa <iam@kantacky.com>
2025-10-18 17:26:15 +0900

    Add widget extension

---
+++ NemulertWidget/NemulertWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  NemulertWidgetBundle.swift
+//  NemulertWidget
+//
+//  Created by Kanta Oikawa on 2025/10/18.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NemulertWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        NemulertWidget()
+        NemulertWidgetControl()
+        NemulertWidgetLiveActivity()
+    }
+}
---
+++ NemulertWidget/NemulertWidgetControl.swift
@@ -0,0 +1,77 @@
+//
+//  NemulertWidgetControl.swift
+//  NemulertWidget
+//
+//  Created by Kanta Oikawa on 2025/10/18.
+//
+
+import AppIntents
+import SwiftUI
+import WidgetKit
+
+struct NemulertWidgetControl: ControlWidget {
+    static let kind: String = "co.furari.Nemulert.NemulertWidget"
+
+    var body: some ControlWidgetConfiguration {
+        AppIntentControlConfiguration(
+            kind: Self.kind,
+            provider: Provider()
+        ) { value in
+            ControlWidgetToggle(
+                "Start Timer",
+                isOn: value.isRunning,
+                action: StartTimerIntent(value.name)
+            ) { isRunning in
+                Label(isRunning ? "On" : "Off", systemImage: "timer")
+            }
+        }
+        .displayName("Timer")
+        .description("A an example control that runs a timer.")
+    }
+}
+
+extension NemulertWidgetControl {
+    struct Value {
+        var isRunning: Bool
+        var name: String
+    }
+
+    struct Provider: AppIntentControlValueProvider {
+        func previewValue(configuration: TimerConfiguration) -> Value {
+            NemulertWidgetControl.Value(isRunning: false, name: configuration.timerName)
+        }
+
+        func currentValue(configuration: TimerConfiguration) async throws -> Value {
+            let isRunning = true // Check if the timer is running
+            return NemulertWidgetControl.Value(isRunning: isRunning, name: configuration.timerName)
+        }
+    }
+}
+
+struct TimerConfiguration: ControlConfigurationIntent {
+    static let title: LocalizedStringResource = "Timer Name Configuration"
+
+    @Parameter(title: "Timer Name", default: "Timer")
+    var timerName: String
+}
+
+struct StartTimerIntent: SetValueIntent {
+    static let title: LocalizedStringResource = "Start a timer"
+
+    @Parameter(title: "Timer Name")
+    var name: String
+
+    @Parameter(title: "Timer is running")
+    var value: Bool
+
+    init() {}
+
+    init(_ name: String) {
+        self.name = name
+    }
+
+    func perform() async throws -> some IntentResult {
+        // Start the timer…
+        return .result()
+    }
+}
