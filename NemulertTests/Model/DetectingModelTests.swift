fa76b44ec279b7f8a84ed33f559742a35e2d037e
Kanta Oikawa <iam@kantacky.com>
2025-11-01 10:08:18 +0900

    Add test cases (not implemented)

--- NemulertTests/Model/DetectingModelTests.swift
+++ NemulertTests/Model/DetectingModelTests.swift
@@ -12,6 +12,7 @@ import Foundation
 import Testing
 
 struct DetectingModelTests {
+    /// 初期化された場合
     @MainActor
     @Test func testInit() async throws {
         let model = withDependencies {
@@ -60,6 +61,7 @@ struct DetectingModelTests {
         #expect(model.dozingCount == 0)
     }
 
+    /// 画面が表示された場合
     @MainActor
     @Test func testOnAppear() async throws {
         let model = withDependencies {
@@ -108,5 +110,41 @@ struct DetectingModelTests {
         #expect(model.motions == [])
         #expect(model.dozing == .idle)
         #expect(model.dozingCount == 0)
+    }
+
+    /// ヘッドフォンが接続された場合
+    @MainActor
+    @Test func testOnHeadphoneConnected() async throws {
+        // TODO: Implement
+    }
+
+    /// ヘッドフォンの接続が切断された場合
+    @MainActor
+    @Test func testOnHeadphoneDisconnected() async throws {
+        // TODO: Implement
+    }
+
+    /// 1個のモーションデータが検出された場合
+    @MainActor
+    @Test func testOn1MotionsStreamed() async throws {
+        // TODO: Implement
+    }
+
+    /// 150個のモーションデータが検出された場合
+    @MainActor
+    @Test func testOn150MotionsStreamed() async throws {
+        // TODO: Implement
+    }
+
+    /// 1度の居眠りが検知された場合
+    @MainActor
+    @Test func testOnDozingDetecting1Time() async throws {
+        // TODO: Implement
+    }
+
+    /// 2度の居眠りが検知された場合
+    @MainActor
+    @Test func testOnDozingDetecting2Times() async throws {
+        // TODO: Implement
     }
 }
